fa45f3b95e21bb80882e3fc14391d01b798650a5
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2022-03-09 23:39:28 +0000

    Implementing error functionality

--- src/components/TabbedComponents/TabbedComponents.tsx
+++ src/components/TabbedComponents/TabbedComponents.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactChild, ReactElement, ReactNode, useEffect, useState } from 'react';
 import TabbedComponentsTab from '../TabbedComponentsTab/';
 
 export interface TabbedComponentsProps {
@@ -9,16 +9,26 @@ export interface TabbedComponentsProps {
 
 function TabbedComponents(props: TabbedComponentsProps): ReactElement {
   const [activeTab, setActiveTab] = useState(0);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const { customClassName = 'TabbedComponents'} = props;
+
+  useEffect(() => {
+    if (props.tabNames.length !== props.children.length) {
+      setError(true);
+      setErrorMessage('The length of the tabNames array needs to match the number of children.');
+    }
+  },[]);
+
   return (
     <div className={customClassName}>
-      <div className={`${customClassName}__tabs`}>
+      {error ? <p>{errorMessage}</p> : <><div className={`${customClassName}__tabs`}>
         {props.tabNames.map((tabName, index) => (
         <TabbedComponentsTab customClassName={customClassName} key={index} activeTab={index === activeTab} setActiveTab={setActiveTab} index={index} tabName={tabName}/>))}
       </div>
       <div className={`${customClassName}__component`}>
         {props.children[activeTab]}
-      </div>
+      </div></>}
     </div>
   )
 }
