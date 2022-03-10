b0786f2736117a218f4aa6b8bbb0526085277cdd
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2022-03-10 12:23:16 +0000

    Introducing testing script

---
+++ src/components/TabbedComponents/TabbedComponents.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import TabbedComponents from "./TabbedComponents";
+
+describe("TabbedComponents", () => {
+  test("renders the TabbedComponents component with a single child", () => {
+    render(
+      <TabbedComponents tabNames={['Test']}>
+        <p>Testing 1, 2, 3</p>
+      </TabbedComponents>
+    );
+  });
+});
+
+describe("TabbedComponents", () => {
+  test("renders the TabbedComponents component with multiple children", () => {
+    render(
+      <TabbedComponents tabNames={['Test','Test 2']}>
+        <p>Testing 1, 2, 3</p>
+        <p>Testing 4, 5, 6</p>
+      </TabbedComponents>
+    );
+  });
+});
