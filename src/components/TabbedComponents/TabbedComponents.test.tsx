f96a0697a3e37070759802f37c1d14e9c327a1fc
Gavin Sykes <gavin@gavinsykes.uk>
2024-01-22 11:53:29 +0000

    Test improvement

--- src/components/TabbedComponents/TabbedComponents.test.tsx
+++ src/components/TabbedComponents/TabbedComponents.test.tsx
@@ -4,15 +4,17 @@ import TabbedComponents from './TabbedComponents';
 
 describe('TabbedComponents', () => {
   test('renders the TabbedComponents component with a single tab', () => {
-    render(
-      <TabbedComponents>
-        <TabbedComponents.TabsList>
-          <TabbedComponents.Tab>Tab 1</TabbedComponents.Tab>
-        </TabbedComponents.TabsList>
-        <TabbedComponents.Display>
-          <p>Testing 1, 2, 3</p>
-        </TabbedComponents.Display>
-      </TabbedComponents>
-    );
+    expect(() => {
+      render(
+        <TabbedComponents>
+          <TabbedComponents.TabsList>
+            <TabbedComponents.Tab>Tab 1</TabbedComponents.Tab>
+          </TabbedComponents.TabsList>
+          <TabbedComponents.Display>
+            <p>Testing 1, 2, 3</p>
+          </TabbedComponents.Display>
+        </TabbedComponents>
+      );
+    }).not.toThrow();
   });
 });
